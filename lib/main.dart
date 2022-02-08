ef0aabbad25f42880a5805ae531406f4e80b38ac
Ohgyuchan <gyu3637@gmail.com>
2022-02-09 00:14:06 +0900

    FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,79 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-import 'controllers/app_controller.dart';
+import 'notification.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  await init();
+  runApp(const MyApp());
+}
+
+Future init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-  final AppController c = Get.put(AppController());
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: FutureBuilder(
-          future: c.initialize(), // 여기서 앱 실행 전에 해야 하는 초기화 진행
-          builder: (context, snapshot) {
-            if (snapshot.hasData &&
-                snapshot.connectionState == ConnectionState.done) {
-              return Center(
-                  child: Obx(() => Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(c.message.value?.notification?.title ?? 'title',
-                              style: TextStyle(fontSize: 20)),
-                          Text(c.message.value?.notification?.body ?? 'message',
-                              style: TextStyle(fontSize: 15)),
-                        ],
-                      )));
-            } else if (snapshot.hasError) {
-              return Center(child: Text('failed to initialize'));
-            } else {
-              return Center(child: Text('initializing...'));
-            }
-          },
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String notificationTitle = 'No Title';
+  String notificationBody = 'No Body';
+  String notificationData = 'No Data';
+
+  @override
+  void initState() {
+    final firebaseMessaging = FCM();
+    firebaseMessaging.setNotifications();
+
+    firebaseMessaging.streamCtlr.stream.listen(_changeData);
+    firebaseMessaging.bodyCtlr.stream.listen(_changeBody);
+    firebaseMessaging.titleCtlr.stream.listen(_changeTitle);
+
+    super.initState();
+  }
+
+  _changeData(String msg) => setState(() => notificationData = msg);
+  _changeBody(String msg) => setState(() => notificationBody = msg);
+  _changeTitle(String msg) => setState(() => notificationTitle = msg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Flutter Notification Details",
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "Notification Title:-  $notificationTitle",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              "Notification Body:-  $notificationBody",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ],
         ),
       ),
     );
